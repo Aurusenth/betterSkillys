189a759374532b11fee040676e4cfdc4978ae319
Auru <aurusenth@gmail.com>
2026-05-03 19:44:49 +0200

    /give updates
    - items now drop in a bag
    - can now spawn x amount of items

--- source/WorldServer/core/commands/admin/Command.Give.cs
+++ source/WorldServer/core/commands/admin/Command.Give.cs
@@ -1,5 +1,10 @@
 ﻿using Shared;
+using Shared.database.character.inventory;
+using Shared.resources;
+using System.Collections.Generic;
+using System.Linq;
 using WorldServer.core.objects;
+using WorldServer.core.objects.containers;
 using WorldServer.core.worlds;
 
 namespace WorldServer.core.commands
@@ -14,31 +19,69 @@ namespace WorldServer.core.commands
             protected override bool Process(Player player, TickTime time, string args)
             {
                 var gameData = player.GameServer.Resources.GameData;
-                if (!gameData.DisplayIdToObjectType.TryGetValue(args, out ushort objType))
+                var splitArgs = args.Split(' ');
+                int amount = 1;
+                string itemName = args;
+
+                if (splitArgs.Length > 1 && int.TryParse(splitArgs[0], out int requestedAmount))
                 {
-                    if (!gameData.IdToObjectType.TryGetValue(args, out objType))
+                    amount = requestedAmount;
+                    itemName = string.Join(" ", splitArgs.Skip(1));
+                }
+
+                if (!gameData.DisplayIdToObjectType.TryGetValue(itemName, out ushort objType))
+                {
+                    if (!gameData.IdToObjectType.TryGetValue(itemName, out objType))
                     {
-                        player.SendError($"unable to find item: {args}!");
+                        player.SendError($"unable to find item: {itemName}!");
                         return false;
                     }
                 }
 
                 if (!gameData.Items.ContainsKey(objType))
                 {
-                    player.SendError($"unable to find item: {args}!");
+                    player.SendError($"unable to find item: {itemName}!");
                     return false;
                 }
 
                 var item = gameData.Items[objType];
-                var availableSlot = player.Inventory.GetAvailableInventorySlot(item);
-                if (availableSlot != -1)
+
+                // 1. Pobieramy ID dla Soulbound Bag (zazwyczaj 0x0503)
+                // W Twoim systemie "Loot Bag 6" to White Bag, "Loot Bag 5" to fioletowy (SB).
+                if (!gameData.IdToObjectType.TryGetValue("Loot Bag 5", out var bagType))
                 {
-                    player.Inventory[availableSlot] = item;
-                    return true;
+                    bagType = 0x0503; // Fallback
                 }
 
-                player.SendError("Not enough space in inventory!");
-                return false;
+                // 2. Tworzymy kontener dokładnie tak, jak robi to Twoja metoda DropBag
+                var container = new Container(player.GameServer, bagType, 120000, true);
+
+                // 3. Wypełniamy ekwipunek kontenera
+                for (int j = 0; j < amount && j < 8; j++)
+                {
+                    // Obsługa stackowalnych przedmiotów (zgodnie z Twoim kodem Loot.cs)
+                    if (item.Quantity > 0 && item.QuantityLimit > 0)
+                    {
+                        container.Inventory.Data[j] = new ItemData()
+                        {
+                            Stack = item.Quantity,
+                            MaxStack = item.QuantityLimit
+                        };
+                    }
+                    container.Inventory[j] = item;
+                }
+
+                // 4. Przypisujemy właściciela (Twoje AccountId w tablicy)
+                container.BagOwners = new int[] { player.AccountId };
+
+                // 5. Ustawiamy pozycję na graczu
+                container.Move(player.X, player.Y);
+
+                // 6. Spawnujemy w świecie (używając metody, którą Twój Loot.cs potwierdził: World.EnterWorld)
+                player.World.EnterWorld(container);
+
+                player.SendInfo($"Gave {amount}x {itemName} in a Soulbound Bag.");
+                return true;
             }
         }
     }
